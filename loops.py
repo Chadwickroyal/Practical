fe612b88554ea36ba0f91ae612c5577ccdd37645
Chadwick Royal <chadwickroyal001@gmail.com>
2023-08-28 20:04:27 +0100

    my subject assignment

---
+++ loops.py
@@ -0,0 +1,5 @@
+#Create a list of 10 subjects
+
+my_list = ["maths", "english", "chemistry", "physics", "geograpy", "agriculture", "biology", "french", "CRS", "further maths"]
+for m in my_list:
+    print(m)
