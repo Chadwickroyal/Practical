d7aa9c5492119d8e764f5986c2c602a6b29ff63a
Chadwick Royal <chadwickroyal001@gmail.com>
2023-09-01 19:10:06 +0100

    my python script assignment

---
+++ script.py
@@ -0,0 +1,43 @@
+# Write a python script that prints exactly "Programming is like building a multilingualpuzzle.
+
+script = "Programming is like building a multilingual puzzle"
+print(script)
+
+# Complete the source code in order to print the integer stored in the variable number, followed by Battery street
+
+number = 98
+print(f"{98}, Battery street")
+
+# Complete the source code in order to print the float stored in the variable number with a precision of 2 digits.
+
+number = 3.14159
+print(f"Float: {number:.2f}")
+
+#Complete the source code in order to print 3 times a string stored in the variable str
+
+str = "Holberton School"
+print(f"{3 * str}{str[:9]}")
+
+#Complete the source code to print Welcome to Holberton School!
+
+str1 = "Holberton"
+str2 = "School"
+str3 = f"{str1} {str2}"
+print(f"welcome to {str3}!")
+
+#Complete the source code
+
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
+
+#Complete the source code to print the object oriented programming with Phyton
+
+description = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+print(description)
